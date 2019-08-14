e336d5bc87b45c17034008f3234f12b2b92a93e1
Kristea Gamel <krgamel@gmail.com>
2019-08-14 13:36:41 -0500

    Design Code
    Reac Native project.

---
+++ components/card.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+import styled from "styled-components";
+
+const Card = props => (
+  <Container>
+    <Cover>
+      <Image source={require("../assets/background2.jpg")} />
+      <Title>Loose Weight</Title>
+    </Cover>
+    <Content>
+      <Logo source={require("../assets/logo-react.png")} />
+      <Caption>React Native</Caption>
+      <Subtitle>5 of 12 sections</Subtitle>
+    </Content>
+  </Container>
+);
+
+export default Card;
+
+const Container = styled.View`
+  background: white;
+  width: 315px;
+  height: 280px;
+  border-radius: 14px;
+  margin-left: 20px;
+  margin-top: 20px;
+  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.15);
+`;
+
+const Cover = styled.View`
+  width: 100%;
+  height: 200px;
+  border-top-left-radius: 14px;
+  border-top-right-radius: 14px;
+  overflow: hidden;
+`;
+
+const Image = styled.Image`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+const Title = styled.Text`
+  color: white;
+  font-size: 24px;
+  font-weight: bold;
+  margin-top: 20px;
+  margin-left: 20px;
+  width: 170px;
+`;
